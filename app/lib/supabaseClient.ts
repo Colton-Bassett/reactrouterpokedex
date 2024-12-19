72d48986de89d9f3651903ea4764bc7015c70a9a
Colton Bassett <coltonbassett@gmail.com>
2024-12-19 11:34:27 -0800

    Updated supabaseClient keys

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
-// const supabaseKey = import.meta.env.VITE_PRIVATE_SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = "https://ioyufxxbkmdjmesonctf.supabase.co";
+const supabasePublicKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlveXVmeHhia21kam1lc29uY3RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2MzU5MzgsImV4cCI6MjA1MDIxMTkzOH0.T_hvPNJt1nINt5mkC_3YMe2gBsszpYheN40zMBKOTFU";
+// const supabasePrivateKey = import.meta.env.VITE_PRIVATE_SUPABASE_SERVICE_ROLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabasePublicKey);
